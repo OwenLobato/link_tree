c46c9ba4740046f834536923a4585e39bcaa227c
OwenLobato <owenlobatov@gmail.com>
2023-12-18 15:04:29 -0600

    Add data to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,19 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
   username: {
     type: String,
     required: [true, 'Please provide a username'],
+    unique: true,
   },
   email: {
     type: String,
@@ -22,8 +32,45 @@ const userSchema = new mongoose.Schema({
   },
   category: {
     type: String,
+    enum: ['Creator', 'Brand', 'Agency', 'Admin'],
+    default: 'Creator',
     required: [true, 'Please provide a category'],
   },
+  links: [
+    {
+      url: {
+        type: String,
+      },
+      title: {
+        type: String,
+      },
+      icon: {
+        type: String,
+      },
+    },
+  ],
+  socialMedia: [
+    {
+      facebook: {
+        type: String,
+      },
+      twitter: {
+        type: String,
+      },
+      instagram: {
+        type: String,
+      },
+      youtube: {
+        type: String,
+      },
+      linkedIn: {
+        type: String,
+      },
+      gitHub: {
+        type: String,
+      },
+    },
+  ],
 });
 
 const User = mongoose.model('users', userSchema);
