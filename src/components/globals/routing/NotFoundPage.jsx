7156e4560873d3d51a26f4d801d5bcf0311002d3
OwenLobato <owenlobatov@gmail.com>
2023-12-28 11:27:34 -0600

    Add styles to page not found

--- src/components/globals/routing/NotFoundPage.jsx
+++ src/components/globals/routing/NotFoundPage.jsx
@@ -1,13 +1,29 @@
 import { Link } from 'react-router-dom';
+import styles from '../../../styles/apply.module.css';
 
 export const NotFoundPage = () => {
   return (
-    <div>
-      <div>
-        <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
-        <Link to='/'>Retun to index</Link>
+    <section
+      className={`${styles.background} min-h-screen flex justify-center items-center`}
+    >
+      <div className='main'>
+        <div className='content bg-white border-2 px-4 py-8 rounded-2xl shadow-lg'>
+          <h1 className='text-2xl font-bold text-center text-red-500'>
+            404 - Page Not Found
+          </h1>
+          <p className='text-center mt-3'>
+            Sorry, the page you are looking for does not exist.
+          </p>
+          <div className='flex flex-col gap-4 text-lg mt-5'>
+            <Link
+              to='/'
+              className='bg-indigo-600 text-white py-2 rounded-lg cursor-pointer text-center hover:bg-indigo-500 transition-all duration-200'
+            >
+              Return to home
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
